812bcdf990b63e90120b6bb92bb9c615dfbc36ac
Kevin Leahey <kleahey@commonapp.org>
2018-05-12 12:55:10 -0400

    For loop example

--- Sum3And5/src/com/kevinleahey/Main.java
+++ Sum3And5/src/com/kevinleahey/Main.java
@@ -3,6 +3,28 @@ package com.kevinleahey;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        // Create a for statement using a range of numbers from 1 to 1000 inclusive
+        // Sum all the numbers that can be divided with both 3 and also with 5
+        // For those numbers that met the above conditions, print out the number.
+        // break out of the loop once you find 5 numbers that met the above conditions
+        // After breaking out of the loop print the sum of the numbers that met the above conditions
+
+        int counter = 0;
+        int sum = 0;
+
+        for(int i = 1; i < 1001; i++) {
+
+            if (counter >= 5) break;
+
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Number is: " + i);
+                counter++;
+                sum += i;
+            }
+
+        }
+
+        System.out.println("Sum of the numbers is " + sum);
     }
 }
